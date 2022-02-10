bd3b4909ca10206da21123b145c377b9464c6065
ATOMICD3V <97807507+ATOMICD3V@users.noreply.github.com>
2022-02-10 06:24:52 -0600

    Delete changefav1.js

--- changefav1.js
+++
@@ -1,7 +0,0 @@
-var link = document.querySelector("link[rel~='icon']");
-if (!link) {
-    link = document.createElement('link');
-    link.rel = 'icon';
-    document.getElementsByTagName('head')[0].appendChild(link);
-}
-link.href = 'pack_icon.png';
