8fc9249ff98dd5e446ab5d0df61e2db195be441a
ATOMICD3V <97807507+ATOMICD3V@users.noreply.github.com>
2022-02-10 06:18:46 -0600

    Create changefav2.js

---
+++ changefav2.js
@@ -0,0 +1,8 @@
+
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = 'gc.png';
